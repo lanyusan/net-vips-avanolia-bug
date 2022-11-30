43dd1619d79366c8fbeecc7b35f5c2ea2a02f861
fqye <fqye@yizhanyun.cn>
2022-11-30 15:57:49 +0800

    wip

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Drawing.Drawing2D;
 using NetVips;
 using System;
+using System.Collections.Generic;
+using Avalonia.Animation;
 
 namespace NetVipsAvalonia
 {
@@ -15,41 +17,117 @@ namespace NetVipsAvalonia
             InitializeComponent();
             GenBtn.Click += async (s, e) =>
             {
-                StateLabel.Content = "Started...";
-                var imageFiles = Directory.GetFiles("images");
+                var openFolderWindow = new OpenFolderDialog();
+                var folder = await openFolderWindow.ShowAsync(this);
 
-
-                var frames = imageFiles.Select(filename => {
-                    var image = NetVips.Image.NewFromFile(filename);
-                    var bytes = image.PngsaveBuffer();
-                    var imageStream = new MemoryStream(bytes);
-                    var bitMap =  new System.Drawing.Bitmap(imageStream);
-                    return image;
-                }).ToArray();
-                var delays = frames.Select( _ => 10).ToArray();
-                var framesJoined = NetVips.Image.Arrayjoin(frames, 1);
-                var height = frames[0].Height;
-                var framesWithMeta = framesJoined.Mutate(mutable =>
+                if(folder is null)
                 {
-                    // Set the number of loops, libvips uses iterations like this:
-                    // 0 - set 0 loops (infinite)
-                    // 1 - loop once
-                    // 2 - loop twice etc.
-                    mutable.Set(GValue.GIntType, "loop", 0);
+                    return;
+                }
+                StateLabel.Content = "Started...";
 
-                    // Set the frame delay(s)
-                    // We use a 1000ms delay for all frames in this example
-                    mutable.Set(GValue.ArrayIntType, "delay", delays);
+                var imageFiles = Directory.GetFiles(folder);
 
-                    // Set page height
-                    mutable.Set(GValue.GIntType, "page-height", height);
-                });
-                // frames.Pngsave(fileName + "-joined.png");
+                var pngs = imageFiles.Where(filename => filename.EndsWith("png")).Select(filename => NetVips.Image.NewFromFile(filename)).ToArray();
+                var framesCount = 0;
+                var gifFiles = imageFiles.Where(filename => filename.EndsWith("gif"));
+
+                List<NetVips.Image[]> gifsList = new();
+
+                foreach(var filename in gifFiles)
+                {
+                    var gif = NetVips.Image.Gifload(filename, -1);
+                    Debug.WriteLine("Gif bands ==> " + gif.Bands);
+                    var pages = (int)(gif.Height / gif.PageHeight);
+                    var frames = new List<NetVips.Image>();
+                    for (var i = 0; i < pages; i++)
+                    {
+                        var frame = gif.Crop(0, i * gif.PageHeight, gif.Width, gif.PageHeight);
+                        frames.Add(frame);
+                    }
+                    if (framesCount == 0)
+                    {
+                        framesCount = frames.Count;
+                    }
+                    else
+                    {
+                        framesCount = Math.Min(framesCount, frames.Count);
+                    }
+                    gifsList.Add(frames.ToArray());
+                }
+                var gifs = gifsList.ToArray();
+
+                var delays = new List<int>();
+                var generatedFramesList = new List<NetVips.Image>();
+
+                for(var i =0; i < framesCount; i++ )
+                {
+                    delays.Add(10);
+                    var baseImage = pngs[0];
+                    var forLayering = new List<NetVips.Image>();
+                    var blendModes = new List<Enums.BlendMode>();
+                    for (var j = 1; j < pngs.Length; j++)
+                    {
+                        forLayering.Add(pngs[j]);
+                        blendModes.Add(Enums.BlendMode.Over);
+                    }
+
+                    foreach (var gif in gifs)
+                    {
+                        forLayering.Add(gif[i]);
+                        blendModes.Add(Enums.BlendMode.Add);
+                    }
+
+                    var blendedFrame = baseImage.Composite(forLayering.ToArray(), blendModes.ToArray());
+                    generatedFramesList.Add(blendedFrame);
+                    delays.Add(10);
+                }
+
+                var generatedFrames = generatedFramesList.ToArray();
+
+                var framesJoined = NetVips.Image.Arrayjoin(generatedFrames, 1);
+
                 DateTimeOffset now = (DateTimeOffset)DateTime.UtcNow;
-                var filename = now.ToUnixTimeMilliseconds() + ".gif";
-                var filepath = Path.Join("images", filename);
-                framesWithMeta.Gifsave(filepath, dither: 0.5, effort: 1, bitdepth: 8);
-                StateLabel.Content = filepath;
+
+
+/*                var compositModes = Enum.GetValues(typeof(Enums.BlendMode))
+                            .Cast<Enums.BlendMode>();
+*/
+
+                var compositModes = new List<Enums.BlendMode>() { Enums.BlendMode.Over };
+
+                foreach(var mode in compositModes)
+                {
+                    var outputFileName = now.ToUnixTimeMilliseconds() + "-" + mode + ".gif";
+
+                    var filepath = Path.Join(folder, "out", outputFileName);
+
+                    var filepathJoined = filepath  + "-joined.png";
+                    framesJoined.Pngsave(filepathJoined);
+                    var height = generatedFrames[0].Height;
+                    var framesWithMeta = framesJoined.Mutate(mutable =>
+                    {
+                        // Set the number of loops, libvips uses iterations like this:
+                        // 0 - set 0 loops (infinite)
+                        // 1 - loop once
+                        // 2 - loop twice etc.
+                        mutable.Set(GValue.GIntType, "loop", 0);
+
+                        // Set the frame delay(s)
+                        // We use a 1000ms delay for all frames in this example
+                        mutable.Set(GValue.ArrayIntType, "delay", delays.ToArray());
+
+                        // Set page height
+                        mutable.Set(GValue.GIntType, "page-height", height);
+                    });
+
+
+                    framesWithMeta.Gifsave(filepath, dither: 0.5, effort: 1, bitdepth: 8);
+                    var filepathJoinedWithMeta = filepath + "-withmeta-joined.png";
+                    framesWithMeta.Pngsave(filepathJoinedWithMeta);
+                }
+
+                StateLabel.Content = "Wrote to folder: " + Path.Join(folder, "out");
             };
         }
 
